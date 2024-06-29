36b8dea142771b1f208b50ff323a0e50e2722160
Lucas Almeida <72702201+zzzluquinhas@users.noreply.github.com>
2024-06-29 18:01:00 -0300

    Commit 2 - Replace Temp with Query

--- index.js
+++ index.js
@@ -8,41 +8,44 @@ function gerarFaturaStr (fatura, pecas) {
 						{ style: "currency", currency: "BRL",
 						minimumFractionDigits: 2 }).format;
 
+	function getPeca(apresentacao){
+		return pecas[apresentacao.id];
+	}
+
 	for (let apre of fatura.apresentacoes) {
-		const peca = pecas[apre.id];
-		function calcularTotalApresentacao(apre, peca){
+		function calcularTotalApresentacao(apresentacao){
 			let total = 0;
 
-			switch (peca.tipo) {
+			switch (getPeca(apresentacao).tipo) {
 				case "tragedia":
 					total = 40000;
-					if (apre.audiencia > 30) {
-						total += 1000 * (apre.audiencia - 30);
+					if (apresentacao.audiencia > 30) {
+						total += 1000 * (apresentacao.audiencia - 30);
 					}
 					break;
 				case "comedia":
 					total = 30000;
-					if (apre.audiencia > 20) {
-						total += 10000 + 500 * (apre.audiencia - 20);
+					if (apresentacao.audiencia > 20) {
+						total += 10000 + 500 * (apresentacao.audiencia - 20);
 					}
-					total += 300 * apre.audiencia;
+					total += 300 * apresentacao.audiencia;
 					break;
 				default:
-					throw new Error(`Peça desconhecia: ${peca.tipo}`);
+					throw new Error(`Peça desconhecia: ${getPeca(apresentacao).tipo}`);
 			}
 			
 			return total;
 		}
 
-		let total = calcularTotalApresentacao(apre, peca);
+		let total = calcularTotalApresentacao(apre);
 	
 		// créditos para próximas contratações
 		creditos += Math.max(apre.audiencia - 30, 0);
-		if (peca.tipo === "comedia") 
+		if (getPeca(apre).tipo === "comedia") 
 			creditos += Math.floor(apre.audiencia / 5);
 	
 		// mais uma linha da fatura
-		faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+		faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
 		totalFatura += total;
 	}
 	faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
